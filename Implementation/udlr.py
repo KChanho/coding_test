79ad1a4b5b474f44b084bc226c6f980cd0b58c0f
KChanho <94108712+KChanho@users.noreply.github.com>
2022-07-21 17:31:39 +0900

    Update udlr.py

--- Implementation/udlr.py
+++ Implementation/udlr.py
@@ -17,7 +17,13 @@ dy = [-1, 1, 0, 0]
 x, y = 1, 1
 
 for d in data:
-  
+  for i in range(len(direction)):
+    if d == direction[i]:
+      nx = x + dx[i]
+      ny = y + dy[i]
+  if nx < 1 or ny < 1 or nx > n or ny > n:
+    continue
+  x, y = nx, ny
 
 print(x, y)
 
